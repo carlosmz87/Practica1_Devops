c4725cc53e2ea7efee3209311ca1f65de3131ab7
carlosmz87 <carlosmartz1995@gmail.com>
2021-12-04 13:09:17 -0600

    add providers

---
+++ infrastructure/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+      Environment = "Produccion"
+      Owner = "Carlos Martinez"
+      Proyect = "Practica1_Devops"
+  }
+}
